31a3e8576ddbad4ec3dab5b792926d6e8d9437a3
martincmr1 <martincmr1@hotmail.com>
2024-11-15 22:35:58 -0300

    "TP4_POO_"

--- src/ECommerce.java
+++ src/ECommerce.java
@@ -1,11 +1,13 @@
 /* Apellido y nombre :RUGNIA CARLOS MARTIN
 DNI: 28337376 
-TP3 : POO 
+TP4 : POO 
  */
 
-import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 import Clases.*;
+import InterfacePago.Pago;
+import Exceptions.CaracteresInvalidosException;
 
 public class ECommerce {
 
@@ -14,149 +16,175 @@ public class ECommerce {
         Usuario usuario = null;
         Carrito carrito = null;
 
-        // Lista de productos disponibles
-        ArrayList<Producto> listaProductosDisponibles = new ArrayList<>();
-        listaProductosDisponibles.add(new Producto(1, "Zapatilla Deportiva", "Nike", "Deportiva", 10, 320000.50));
-        listaProductosDisponibles.add(new Producto(2, "Zapatilla Running", "Adidas", "Running", 15, 150000.60));
-        listaProductosDisponibles.add(new Producto(3, "Zapatilla Casual", "Puma", "Casual", 8, 210000.80));
-        listaProductosDisponibles.add(new Producto(4, "Zapatilla de Basquet", "Jordan", "Basquet", 5, 350000.60));
-        listaProductosDisponibles.add(new Producto(5, "Zapatilla Skate", "Vans", "Skate", 12, 410000.80));
-        listaProductosDisponibles.add(new Producto(6, "Zapatilla de Trekking", "Salomon", "Trekking", 6, 125650.90));
-        listaProductosDisponibles.add(new Producto(7, "Zapatilla de Futbol", "Nike", "Futbol", 10, 388000.40));
-        listaProductosDisponibles.add(new Producto(8, "Zapatilla Retro", "Reebok", "Retro", 7, 110000.75));
-        listaProductosDisponibles.add(new Producto(9, "Zapatilla de CrossFit", "Under Armour", "CrossFit", 4, 165000.10));
-        listaProductosDisponibles.add(new Producto(10, "Zapatilla de Correr", "New Balance", "Correr", 9, 180000.25));
+         GestorProductos gestorProductos = new GestorProductos();
 
         // Menú interactivo
         while (true) {
-            System.out.println("-----------------------");
-            System.out.println("----- Deport Line -----");
-            System.out.println("-----------------------");
-            System.out.println("1. Registrarse");
-            System.out.println("2. Iniciar sesion");
-            System.out.println("3. Ver productos disponibles");
-            System.out.println("4. Ver productos con descuento");
-            System.out.println("5. Agregar productos al carrito");
-            System.out.println("6. Ver carrito");
-            System.out.println("7. Procesar pago");
-            System.out.println("8. Salir");
-            System.out.print("Seleccione una opcion: ");
-            int opcion = scanner.nextInt();
-            scanner.nextLine(); // Consumir el salto de línea
+            try {
+                System.out.println("-----------------------");
+                System.out.println("----- Deport Line -----");
+                System.out.println("-----------------------");
+                System.out.println("1. Registrarse");
+                System.out.println("2. Iniciar sesión");
+                System.out.println("3. Ver productos disponibles");
+                System.out.println("4. Ver productos con descuento");
+                System.out.println("5. Agregar productos al carrito");
+                System.out.println("6. Ver carrito");
+                System.out.println("7. Procesar pago");
+                System.out.println("8. Salir");
+                System.out.print("Seleccione una opción: ");
+                int opcion;
 
-            switch (opcion) {
-                case 1:
-                    // Registrar usuario
-                    System.out.print("Ingrese su nombre: ");
-                    String nombre = scanner.nextLine();
-                    System.out.print("Ingrese su email: ");
-                    String email = scanner.nextLine();
-                    System.out.print("Ingrese su contrasena: ");
-                    String contrasena = scanner.nextLine();
-                    usuario = new Usuario(1, nombre, email, contrasena);
-                    usuario.registrar();
-                    break;
+                try {
+                    opcion = scanner.nextInt();
+                    scanner.nextLine(); // Consumir el salto de línea
 
-                case 2:
-                    // Iniciar sesión
-                    if (usuario != null) {
+                    if (opcion < 1 || opcion > 8) {
+                        throw new CaracteresInvalidosException("Opción inválida. Por favor, seleccione una opción entre 1 y 8.");
+                    }
+                } catch (InputMismatchException e) {
+                    scanner.nextLine(); // Limpiar el buffer
+                    throw new CaracteresInvalidosException("Entrada inválida. Por favor, ingrese un número válido.");
+                }
+
+                switch (opcion) {
+                    case 1 -> {
+                        // Registrar usuario
+                        System.out.print("Ingrese su nombre: ");
+                        String nombre = scanner.nextLine();
                         System.out.print("Ingrese su email: ");
-                        email = scanner.nextLine();
-                        System.out.print("Ingrese su contrasena: ");
-                        contrasena = scanner.nextLine();
-                        if (usuario.iniciarSesion(email, contrasena)) {
-                            carrito = new Carrito();
-                        }
-                    } else {
-                        System.out.println("Debe registrarse primero.");
+                        String email = scanner.nextLine();
+                        System.out.print("Ingrese su contraseña: ");
+                        String contrasena = scanner.nextLine();
+                        usuario = new Usuario(1, nombre, email, contrasena);
+                        usuario.registrar();
                     }
-                    break;
 
-                case 3:
-                    // Ver productos disponibles
-                    System.out.println("=== Productos disponibles ===");
-                    for (Producto p : listaProductosDisponibles) {
-                        p.verDetalles();  // Mostrar detalles del producto, incluyendo precio con IVA
-                    }
-                    break;
-
-                case 4:
-                    // Ver productos con descuento
-                    System.out.println("=== Productos con descuento ===");
-                    for (Producto p : listaProductosDisponibles) {
-                        double precioConDescuento = p.calcularDescuento();
-                        System.out.println(p.getNombre() + " - Precio con descuento: $" + precioConDescuento);
-                    }
-                    break;
-
-                case 5:
-                    // Agregar productos al carrito
-                    if (carrito != null) {
-                        System.out.print("Ingrese el ID del producto que desea agregar: ");
-                        int idProducto = scanner.nextInt();
-                        Producto productoSeleccionado = null;
-                        for (Producto p : listaProductosDisponibles) {
-                            if (p.getId() == idProducto) {
-                                productoSeleccionado = p;
-                                break;
+                    case 2 -> {
+                        // Iniciar sesión
+                        if (usuario != null) {
+                            System.out.print("Ingrese su email: ");
+                            String emailSesion = scanner.nextLine();
+                            System.out.print("Ingrese su contraseña: ");
+                            String contrasenaSesion = scanner.nextLine();
+                            if (usuario.iniciarSesion(emailSesion, contrasenaSesion)) {
+                                carrito = new Carrito();
                             }
-                        }
-                        if (productoSeleccionado != null) {
-                            carrito.agregarProducto(productoSeleccionado);
-                            System.out.println("Producto agregado al carrito.");
                         } else {
-                            System.out.println("Producto no encontrado.");
+                            System.out.println("Debe registrarse primero.");
                         }
-                    } else {
-                        System.out.println("Debe iniciar sesion primero.");
                     }
-                    break;
 
-                case 6:
-                    // Ver productos en el carrito
-                    if (carrito != null) {
-                        System.out.println("=== Productos en el Carrito ===");
-                        for (Producto p : carrito.getProductos()) {
-                            p.verDetalles();  // Mostrar detalles, incluyendo precio con IVA
+                    case 3 -> {
+                        // Ver productos disponibles
+                       System.out.println("=== Productos disponibles ===");
+                        for (Producto p : gestorProductos.getListaProductos()) {
+                            p.verDetalles(); // Mostrar detalles del producto
                         }
-                    } else {
-                        System.out.println("Debe iniciar sesion primero.");
+                        break;
                     }
-                    break;
 
-                case 7:
-                    // Procesar el pago
-                    if (carrito != null && !carrito.getProductos().isEmpty()) {
-                        System.out.println("Procesando pago...");
-                        Pago pago = new PagoConTarjeta();
-                        pago.procesarPago();
-
-                        // Mostrar los productos comprados
-                        System.out.println("Productos comprados:");
-                        double totalGastado = 0;
-                        for (Producto p : carrito.getProductos()) {
-                            p.verDetalles();
-                            totalGastado += p.getPrecio();
+                    case 4 -> {
+                        // Ver productos con descuento
+                       System.out.println("=== Productos con descuento ===");
+                        for (Producto p : gestorProductos.getListaProductos()) {
+                            double precioConDescuento = p.calcularDescuento();
+                            System.out.println(p.getNombre() + " - Precio con descuento: $" + precioConDescuento);
                         }
-
-                        // Mostrar el total gastado
-                        System.out.println("Total gastado: $" + totalGastado);
-                        System.out.println("Pago procesado con exito.");
-
-                        // Limpiar el carrito después del pago
-                        carrito = new Carrito();
-                    } else {
-                        System.out.println("El carrito está vacio. No se puede procesar el pago.");
+                        break;
                     }
-                    break;
 
-                case 8:
-                    // Salir
-                    System.out.println("Gracias por usar Deport Line.");
-                    return;
+                    case 5 -> {
+                        // Agregar productos al carrito
+                         if (carrito != null) {
+                            System.out.print("Ingrese el ID del producto que desea agregar: ");
+                            int idProducto = scanner.nextInt();
+                            Producto productoSeleccionado = gestorProductos.buscarProductoPorId(idProducto);
+                            if (productoSeleccionado != null) {
+                                carrito.agregarProducto(productoSeleccionado);
+                                System.out.println("Producto agregado al carrito.");
+                            } else {
+                                System.out.println("Producto no encontrado.");
+                            }
+                        } else {
+                            System.out.println("Debe iniciar sesión primero.");
+                        }
+                        break;
+                    }
 
-                default:
-                    System.out.println("Opcion no válida. Intente de nuevo.");
+                    case 6 -> {
+                        // Ver productos en el carrito
+                        if (carrito != null) {
+                            System.out.println("=== Productos en el Carrito ===");
+                            for (Producto p : carrito.getProductos()) {
+                                p.verDetalles();
+                            }
+                        } else {
+                            System.out.println("Debe iniciar sesión primero.");
+                        }
+                    }
+
+                    case 7 -> {
+                        // Procesar el pago
+                        if (carrito != null && !carrito.getProductos().isEmpty()) {
+                            System.out.println("Seleccione el método de pago:");
+                            System.out.println("1. Tarjeta");
+                            System.out.println("2. Efectivo");
+
+                            try {
+                                int metodoPago = scanner.nextInt();
+                                scanner.nextLine(); // Consumir el salto de línea
+
+                                Pago pago;
+
+                                if (metodoPago == 1) {
+                                    pago = new PagoConTarjeta();
+                                } else if (metodoPago == 2) {
+                                    pago = new PagoEnEfectivo();
+                                } else {
+                                    System.out.println("Método de pago no válido.");
+                                    break;
+                                }
+
+                                double totalGastado = 0;
+                                for (Producto p : carrito.getProductos()) {
+                                    totalGastado += p.getPrecio();
+                                }
+
+                                if (pago.validarPago(totalGastado)) {
+                                    pago.procesarPago();
+
+                                    System.out.println("Productos comprados:");
+                                    for (Producto p : carrito.getProductos()) {
+                                        p.verDetalles();
+                                    }
+
+                                    System.out.println("Total gastado: $" + totalGastado);
+                                    System.out.println("Pago procesado con éxito.");
+
+                                    carrito = new Carrito(); // Vaciar el carrito
+                                } else {
+                                    System.out.println("El pago no pudo ser validado.");
+                                }
+                            } catch (InputMismatchException e) {
+                                scanner.nextLine(); // Limpiar el buffer
+                                System.out.println("Entrada inválida. Seleccione una opción numérica válida.");
+                            }
+                        } else {
+                            System.out.println("Debe iniciar sesión y tener productos en el carrito para procesar el pago.");
+                        }
+                    }
+
+                    case 8 -> {
+                        // Salir
+                        System.out.println("Gracias por usar Deport Line.");
+                        return;
+                    }
+
+                    default -> System.out.println("Opción no válida.");
+                }
+            } catch (CaracteresInvalidosException e) {
+                System.out.println(e.getMessage());
             }
         }
     }
